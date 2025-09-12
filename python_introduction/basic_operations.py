098fda5b0179903bc3ee7427d2ecdb3933079147
CynthiaKaluson <cynthiakalu1974@gmail.com>
2025-09-12 17:28:21 +0200

    Added basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+# Assign values to two numbers for arithmetic operations
+number_1 = 10
+number_2 = 5
+
+# Print the result of adding number_1 and number_2
+print("Addition of", number_1, "and", number_2, "is", number_1 + number_2)
+
+# Print the result of subtracting number_2 from number_1
+print("Subtraction of", number_1, "and", number_2, "is", number_1 - number_2)
+
+# Print the result of multiplying number_1 and number_2
+print("Multiplication of", number_1, "and", number_2, "is", number_1 * number_2)
