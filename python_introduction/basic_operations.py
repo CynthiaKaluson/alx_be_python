6fadf9dc11a48ce8452e4b5a2eccb419b712ce54
Web3 Princess 👸 💖 <134476536+CynthiaKaluson@users.noreply.github.com>
2025-09-12 17:33:58 +0200

    Update basic_operations.py
    I removed the underscores in the variables

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,12 +1,12 @@
 # Assign values to two numbers for arithmetic operations
-number_1 = 10
-number_2 = 5
+number1 = 10
+number2 = 5
 
 # Print the result of adding number_1 and number_2
-print("Addition of", number_1, "and", number_2, "is", number_1 + number_2)
+print("Addition of", number1, "and", number2, "is", number1 + number2)
 
 # Print the result of subtracting number_2 from number_1
-print("Subtraction of", number_1, "and", number_2, "is", number_1 - number_2)
+print("Subtraction of", number1, "and", number2, "is", number1 - number2)
 
 # Print the result of multiplying number_1 and number_2
-print("Multiplication of", number_1, "and", number_2, "is", number_1 * number_2)
+print("Multiplication of", number1, "and", number2, "is", number1 * number2)
