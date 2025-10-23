bc3d7e2baed3f3d5b6c4bf8e36c0196c63e1f478
CynthiaKaluson <cynthiakalu1974@gmail.com>
2025-10-23 18:44:04 +0100

    Complete OOP project with all tasks

---
+++ oop/library_system.py
@@ -0,0 +1,41 @@
+# library_system.py
+
+class Book:
+    def __init__(self, title, author):
+        """Initialize base book attributes"""
+        self.title = title
+        self.author = author
+
+
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        """Initialize eBook with additional file_size attribute"""
+        super().__init__(title, author)  # Call parent class constructor
+        self.file_size = file_size
+
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        """Initialize print book with additional page_count attribute"""
+        super().__init__(title, author)  # Call parent class constructor
+        self.page_count = page_count
+
+
+class Library:
+    def __init__(self):
+        """Initialize library with empty books list"""
+        self.books = []
+
+    def add_book(self, book):
+        """Add a book to the library"""
+        self.books.append(book)
+
+    def list_books(self):
+        """Print details of all books in the library"""
+        for book in self.books:
+            if isinstance(book, EBook):
+                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
+            elif isinstance(book, PrintBook):
+                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
+            else:
+                print(f"Book: {book.title} by {book.author}")
